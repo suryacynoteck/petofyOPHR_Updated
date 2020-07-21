7cc1452ca2eaf628378083764646ffb59b9144d7
ashish rawat <ashishrawat6677@gmail.com>
2020-07-21 14:11:44 +0530

    Response Request classes Name change.

---
+++ app/src/main/java/com/cynoteck/petofyvet/utils/Methods.java
@@ -0,0 +1,76 @@
+package com.cynoteck.petofyvet.utils;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AlertDialog;
+import com.cynoteck.petofyvet.R;
+public class Methods {
+    private Context c;
+    private Dialog progressBarDialog;
+
+    public Methods(Context c) {
+        this.c = c;
+    }
+
+    public final boolean isInternetOn() {
+        boolean flag = false;
+        ConnectivityManager connec = (ConnectivityManager) c.getSystemService(c.CONNECTIVITY_SERVICE);
+        if (connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ||
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED) {
+            flag = true;
+
+
+        } else if (connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED) {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public void DialogInternet() {
+        AlertDialog.Builder ad = new AlertDialog.Builder(c);
+        ad.setMessage(R.string.CheckInternet);
+        ad.setCancelable(true);
+        ad.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        ad.show();
+    }
+
+    public void showCustomProgressBarDialog(Context context) {
+        try {
+        progressBarDialog = new Dialog(context);
+        progressBarDialog.setContentView(R.layout.custom_spin_progress_dialog);
+        progressBarDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        progressBarDialog.setCancelable(false);
+        progressBarDialog.setCanceledOnTouchOutside(false);
+        progressBarDialog.show();
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        Window window = progressBarDialog.getWindow();
+        lp.copyFrom(window.getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        window.setAttributes(lp);
+        }catch (Exception e) {
+            e.printStackTrace();
+            Log.e("progressDilaog",e.getMessage());
+        }
+    }
+    public void customProgressDismiss() {
+        progressBarDialog.dismiss();
+    }
+
+}
