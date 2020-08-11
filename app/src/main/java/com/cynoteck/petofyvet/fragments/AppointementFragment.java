57fc2c84575d8fb19d56ef22359ed8ddc0bf87e8
amitayuModak <modak.amitayu@gmail.com>
2020-08-11 17:33:23 +0530

    prescription pdf format in appointment fragment_appointement.xml

--- app/src/main/java/com/cynoteck/petofyvet/fragments/AppointementFragment.java
+++ app/src/main/java/com/cynoteck/petofyvet/fragments/AppointementFragment.java
@@ -1,12 +1,21 @@
 package com.cynoteck.petofyvet.fragments;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.print.PrintManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
 
 import com.cynoteck.petofyvet.R;
 
@@ -14,6 +23,9 @@ import com.cynoteck.petofyvet.R;
  * A simple {@link Fragment} subclass.
  */
 public class AppointementFragment extends Fragment {
+    View view;
+    WebView web_invoice;
+    Button pdf_format;
 
     public AppointementFragment() {
         // Required empty public constructor
@@ -24,6 +36,177 @@ public class AppointementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_appointement, container, false);
+        view=inflater.inflate(R.layout.fragment_appointement, container, false);
+        web_invoice=view.findViewById(R.id.web_invoice);
+        pdf_format=view.findViewById(R.id.pdf_format);
+        String name="Aviral";
+        String care="Aviral Care";
+        String mobile="999-788-7756";
+        String mobile1="999-788-7756";
+        String email="aviral.rana@gmail.com";
+        String fora="Rustam";
+        String age="Puppy";
+        String sex="Male";
+        String pet_parent="Pramod Rana";
+        String temparature="98";
+        String Symptons="Problems";
+        String Diagnosis="Running";
+        String Treatment_Remarks="Drontal plus";
+        String next_visit="07/08/2020";
+        String address="Dehradun";
+        String registration_number="VET-00987";
+        String str="<!DOCTYPE html>\n"+
+                    "<html>\n" +
+                "<head>\n" +
+                "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n" +
+                "<script src=\"//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js\"></script>\n" +
+                "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\n" +
+                "    \n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css\">\n" +
+                "\n" +
+                "    <title>Invioce</title>\n" +
+                "</head>\n" +
+                "<style type=\"text/css\">\n" +
+                "    .invoice-title h2, .invoice-title h3 {\n" +
+                "        display: inline-block;\n" +
+                "    }\n" +
+                "\n" +
+                "    .table > tbody > tr > .no-line {\n" +
+                "        border-top: none;\n" +
+                "    }\n" +
+                "\n" +
+                "    .table > thead > tr > .no-line {\n" +
+                "        border-bottom: none;\n" +
+                "    }\n" +
+                "\n" +
+                "    .table > tbody > tr > .thick-line {\n" +
+                "        border-top: 2px solid;\n" +
+                "    }\n" +
+                "    @page {\n" +
+                "      size: A4;\n" +
+                "      margin: 15px;\n" +
+                "    }\n" +
+                "</style>\n" +
+                "\n" +
+                "<body>\n" +
+                "    <div class=\"container\">\n" +
+                "        <p><?=date('d/m/Y')?></p> \n" +
+                "    <div class=\"row\">\n" +
+                "        <div class=\"col-xs-12\">\n" +
+                "            <div class=\"invoice-title \">\n" +
+                "                <div class=\"row\">\n" +
+                "                    <div class=\"col-lg-12 col-md-12 col-xs-12\" style=\"font-size: 25px;font-family: cizel;\">\n" +
+                "                       <b>"+name+"</b> \n" +
+                "                    </div>\n" +
+                "                    <div class=\"col-lg-12 col-md-12 col-xs-12\" style=\"font-size: 20px; margin-bottom: 20px;\">\n" +
+                "                        MBBS,MVS \n" +
+                "                    </div>\n" +
+                "                    <div class=\"col-lg-6 col-md-6 col-xs-6\" style=\"font-size: 20px; \" >\n" +care+
+                "                       \n" +
+                "                    </div>\n" +
+                "                    <div class=\"col-lg-6 col-md-6 col-xs-6 text-right\" style=\"font-size: 20px;\">\n" +
+                "                       <b> Mobile :"+mobile+" </b>\n" +
+                "                    </div>\n" +
+                "                    \n" +
+                "                    <div class=\"col-lg-6 col-md-6 col-xs-6\" style=\"font-size: 17px;\">\n" +
+                "                       <b> Email: "+email+" </b>\n" +
+                "                    </div>\n" +
+                "                    <div class=\"col-lg-6 col-md-6 col-xs-6 text-right\" style=\"font-size: 20px;\">\n" +
+                "                       <b> "+mobile1+"</b>\n" +
+                "                    </div>\n" +
+                "                 \n" +
+                "                    \n" +
+                "                </div>\n" +
+                "               \n" +
+                "                \n" +
+                "            </div>\n" +
+                "            <div class=\"row\">\n" +
+                "                <div class=\"col-md-12\" style=\"border: 1px solid black;\"></div>\n" +
+                "            </div>\n" +
+                "            <div class=\"row\">\n" +
+                "                <div class=\"col-xs-3\" style=\"font-size: 20px;\">\n" +
+                "                    <b>For a: "+fora+"</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-3\" style=\"font-size: 20px;\">\n" +
+                "                    <b>Age: "+age+"</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-3\" style=\"font-size: 20px;\">\n" +
+                "                    <b>Sex: "+sex+"</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-3\" style=\"font-size: 20px; margin-bottom: 25px;\">\n" +
+                "                    <b>Date: <?=date('d/m/Y')?></b>\n" +
+                "                </div>\n" +
+                "\n" +
+                "\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 25px;\">\n" +
+                "                    <b>Pet Parant Name:"+pet_parent+"  ("+mobile+")</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 10px;\">\n" +
+                "                    <b>Temparature(F): "+temparature+"</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 25px;\">\n" +
+                "                    <b>Symptons:</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"margin-bottom: 10px;\">\n" +
+                "                    <p>"+Symptons+"</p>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 25px;\">\n" +
+                "                    <b>Diagnosis:</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 10px;\">\n" +
+                "                    <p>"+Diagnosis+"</p>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 25px;\">\n" +
+                "                    <b>Treatment Remarks:</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 10px;\">\n" +
+                "                    <p>"+Treatment_Remarks+"</p>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 25px;\">\n" +
+                "                    <b>Next Visit:</b>\n" +
+                "                </div>\n" +
+                "                <div class=\"col-xs-12\" style=\"font-size: 20px; margin-bottom: 10px;\">\n" +
+                "                    <p>"+next_visit+"</p>\n" +
+                "                </div>\n" +
+                "\n" +
+                "            </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>\n" +
+                "            <div class=\"col-md-12\" style=\"border: 1px solid black;\"></div>\n" +
+                "            <div class=\"col-md-12\" style=\"font-size: 25px; text-align: center;\">Address: "+address+", Registration Number: "+registration_number+"</div>\n" +
+                "            \n" +
+                "        </div>\n" +
+                "\n" +
+                "\n" +
+                "    </div>\n" +
+                "\n" +
+                "</div>\n" +
+                "<script type=\"text/javascript\">\n" +
+                "    $(function(){\n" +
+                "        window.print();\n" +
+                "        window.close();\n" +
+                "    });\n" +
+                "</script>\n" +
+                "</body>\n" +
+                "</html>";
+        web_invoice.loadDataWithBaseURL(null,str,"text/html","utf-8",null);
+
+        pdf_format=view.findViewById(R.id.pdf_format);
+        pdf_format.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context=getActivity();
+                PrintManager printManager=(PrintManager)getActivity().getSystemService(context.PRINT_SERVICE);
+                PrintDocumentAdapter adapter=null;
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+                    adapter=web_invoice.createPrintDocumentAdapter();
+                }
+                String JobName=getString(R.string.app_name) +"Document";
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+                    PrintJob printJob=printManager.print(JobName,adapter,new PrintAttributes.Builder().build());
+                }
+
+            }
+        });
+        return view;
+
     }
 }
