484b155301568afc8e458c97719399db937e0041
ashish rawat <ashishrawat6677@gmail.com>
2020-08-20 12:17:55 +0530

    Reports Section Completed

---
+++ app/src/main/java/com/cynoteck/petofyvet/response/getPetReportsResponse/AddUpdateDeleteClinicVisitResponse.java
@@ -0,0 +1,51 @@
+/*
+package com.cynoteck.petofyvet.response.getPetReportsResponse;
+
+import com.cynoteck.petofyvet.response.Header;
+import com.cynoteck.petofyvet.response.Response;
+
+public class AddUpdateDeleteClinicVisitResponse {
+    private Data data;
+
+    private Response response;
+
+    private Header header;
+
+    public Data getData ()
+    {
+        return data;
+    }
+
+    public void setData (Data data)
+    {
+        this.data = data;
+    }
+
+    public Response getResponse ()
+    {
+        return response;
+    }
+
+    public void setResponse (Response response)
+    {
+        this.response = response;
+    }
+
+    public Header getHeader ()
+    {
+        return header;
+    }
+
+    public void setHeader (Header header)
+    {
+        this.header = header;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [data = "+data+", response = "+response+", header = "+header+"]";
+    }
+}
+
+*/
