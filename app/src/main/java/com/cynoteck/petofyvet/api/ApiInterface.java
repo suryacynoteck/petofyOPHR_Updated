d8ae2c00f61f2a98d724c4ae105e543076531463
ashish rawat <ashishrawat6677@gmail.com>
2020-09-07 22:24:57 +0530

    Bugs After Pull

--- app/src/main/java/com/cynoteck/petofyvet/api/ApiInterface.java
+++ app/src/main/java/com/cynoteck/petofyvet/api/ApiInterface.java
@@ -10,6 +10,8 @@ import com.cynoteck.petofyvet.params.allStaffRequest.ChangeStaffStatusRequest;
 import com.cynoteck.petofyvet.params.allStaffRequest.StaffDeatilsRequest;
 import com.cynoteck.petofyvet.params.allStaffRequest.UpdateStaffRequest;
 import com.cynoteck.petofyvet.params.appointmentParams.AppointmentsStatusRequest;
+import com.cynoteck.petofyvet.params.appointmentParams.CreateAppointRequest;
+import com.cynoteck.petofyvet.params.appointmentParams.UpdateAppointmentRequest;
 import com.cynoteck.petofyvet.params.changePassRequest.ChangePassRequest;
 import com.cynoteck.petofyvet.params.checkpetInVetRegister.InPetRegisterRequest;
 import com.cynoteck.petofyvet.params.forgetPassRequest.ForgetPassRequest;
@@ -29,6 +31,7 @@ import com.cynoteck.petofyvet.params.updateLapTestParams.UpdateLabTestRequest;
 import com.cynoteck.petofyvet.params.updateRequest.getValue.UpdateParams;
 import com.cynoteck.petofyvet.params.updateRequest.updateParamRequest.UpdatePetRequest;
 import com.cynoteck.petofyvet.params.updateXRayParams.UpdateXrayRequest;
+import com.cynoteck.petofyvet.params.workingHoursParameter.WorkingHoursParameter;
 import com.cynoteck.petofyvet.response.InPetVeterian.InPetVeterianResponse;
 import com.cynoteck.petofyvet.response.addHospitalizationResponse.AddhospitalizationResposee;
 import com.cynoteck.petofyvet.response.addLabWorkResponse.AddLabWorkResponse;
@@ -41,15 +44,19 @@ import com.cynoteck.petofyvet.response.addPet.petSizeResponse.PetSizeValueRespon
 import com.cynoteck.petofyvet.response.addPet.uniqueIdResponse.UniqueResponse;
 import com.cynoteck.petofyvet.response.addPetClinicresponse.AddpetClinicResponse;
 import com.cynoteck.petofyvet.response.addTestAndXRayResponse.AddTestXRayResponse;
+import com.cynoteck.petofyvet.response.appointmentResponse.AppointmentDetailsResponse;
+import com.cynoteck.petofyvet.response.appointmentResponse.CreateAppointmentResponse;
 import com.cynoteck.petofyvet.response.appointmentResponse.GetAppointmentResponse;
 import com.cynoteck.petofyvet.response.clinicVisist.ClinicVisitResponse;
 import com.cynoteck.petofyvet.response.forgetAndChangePassResponse.PasswordResponse;
+import com.cynoteck.petofyvet.response.getAppointmentsStatusResponse.AppointmentStatusResponse;
 import com.cynoteck.petofyvet.response.getLabTestReportResponse.getLabTestReportDetailsResponse.GetLabTestReportDeatilsResponse;
 import com.cynoteck.petofyvet.response.getLabTestReportResponse.getPetLabWorkListResponse.PetLabWorkResponse;
 import com.cynoteck.petofyvet.response.getPetDetailsResponse.GetPetResponse;
 import com.cynoteck.petofyvet.response.getPetHospitalizationResponse.getHospitalizationDeatilsResponse.GetHospitalizationDeatilsResponse;
 import com.cynoteck.petofyvet.response.getPetHospitalizationResponse.getHospitalizationListResponse.GetPetHospitalizationResponse;
 import com.cynoteck.petofyvet.response.getPetIdCardResponse.PetIdCardResponse;
+import com.cynoteck.petofyvet.response.getPetParentResponse.GetPetParentResponse;
 import com.cynoteck.petofyvet.response.getPetReportsResponse.AddUpdateDeleteClinicVisitResponse;
 import com.cynoteck.petofyvet.response.getPetReportsResponse.GetReportsTypeResponse;
 import com.cynoteck.petofyvet.response.getPetReportsResponse.getClinicVisitDetails.GetClinicVisitsDetailsResponse;
@@ -59,6 +66,7 @@ import com.cynoteck.petofyvet.response.getStaffResponse.GetAllStaffResponse;
 import com.cynoteck.petofyvet.response.getStaffResponse.GetStaffDetailsResponse;
 import com.cynoteck.petofyvet.response.getStaffResponse.GetStaffStatusResponse;
 import com.cynoteck.petofyvet.response.getStaffResponse.GetUpdateStaffResponse;
+import com.cynoteck.petofyvet.response.getWorkingHoursResponse.WorkingHoursResponse;
 import com.cynoteck.petofyvet.response.getXRayReports.getPetTestAndXRayResponse.GetPetTestAndXRayResponse;
 import com.cynoteck.petofyvet.response.getXRayReports.getXRayReportDetailsResponse.GetXRayReportDeatilsResponse;
 import com.cynoteck.petofyvet.response.hospitalTypeListResponse.HospitalAddmissionTypeResp;
@@ -67,6 +75,7 @@ import com.cynoteck.petofyvet.response.loginRegisterResponse.LoginRegisterRespon
 import com.cynoteck.petofyvet.response.newPetResponse.NewPetRegisterResponse;
 import com.cynoteck.petofyvet.response.otpResponse.OtpResponse;
 import com.cynoteck.petofyvet.response.recentEntrys.RecentEntrysResponse;
+import com.cynoteck.petofyvet.response.saveWorkingReponse.SaveWorkingHoursResponse;
 import com.cynoteck.petofyvet.response.searchRemaks.SearchRemaksResponse;
 import com.cynoteck.petofyvet.response.testResponse.XrayTestResponse;
 import com.cynoteck.petofyvet.response.updateProfileResponse.CityResponse;
@@ -376,7 +385,38 @@ public interface ApiInterface {
 
     @Headers({ "Content-Type: application/json;charset=UTF-8"})
     @POST("appointment/ApproveRejectAppointment")
-    Call<GetAppointmentResponse> appointmentApproveReject(@Header("Authorization") String auth, AppointmentsStatusRequest appointmentsStatusRequest);
+    Call<AppointmentStatusResponse> appointmentApproveReject(@Header("Authorization") String auth, @Body AppointmentsStatusRequest appointmentsStatusRequest);
+
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    @POST("appointment/CreateAppointment")
+    Call<CreateAppointmentResponse> createAppointment(@Header("Authorization") String auth, @Body CreateAppointRequest createAppointRequest);
+
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    @POST("appointment/UpdateAppointment")
+    Call<CreateAppointmentResponse> updateAppointment(@Header("Authorization") String auth, @Body UpdateAppointmentRequest updateAppointmentRequest);
+
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    @POST("user/GetPetParentList")
+    Call<GetPetParentResponse> getPetParentList(@Header("Authorization") String auth);
+
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    @POST("appointment/GetAppointmentById")
+    Call<AppointmentDetailsResponse> getAppointmentsDetails(@Header("Authorization") String auth,
+                                                            @Body GetPetListRequest addPetRequset);
+
+    //TODO=================GET OPERATING HOURS===========================
+
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    @POST("user/GetOperatingHours")
+    Call<WorkingHoursResponse> getOperatingHours(@Header("Authorization") String auth);
+
+    //TODO==================SAVE OPERATING HOURS===========================
+
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    @POST("user/SaveOperatingHours")
+    Call<SaveWorkingHoursResponse> saveOperatingHours(@Header("Authorization") String auth, @Body WorkingHoursParameter workingHoursParameter);
+
+
 }
 
 
