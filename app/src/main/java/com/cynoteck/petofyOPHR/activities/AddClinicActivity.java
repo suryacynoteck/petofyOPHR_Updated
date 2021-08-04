47fe5620a69df9ffef53c5a5567c59bd6f9317e4
suryakant.uniyal@gmail.com <suryakant.uniyal@gmail.com>
2021-08-04 14:50:37 +0530

    codecleanup

--- app/src/main/java/com/cynoteck/petofyOPHR/activities/AddClinicActivity.java
+++ app/src/main/java/com/cynoteck/petofyOPHR/activities/AddClinicActivity.java
@@ -209,8 +209,6 @@ public class AddClinicActivity extends AppCompatActivity implements View.OnClick
     private ArrayList<String> immunizationDateList;
     String value;
 
-    int lastStaus = 0, natureStaus = 50;
-    //pet_profile_dialog
 
     ImageView dialog_cross_IV,dialog_pet_profile_image_IV;
     TextView dialog_pet_name_TV,dialog_pet_breed_TV ,dialog_pet_age_TV,dialog_pet_gender_TV,dialog_pet_id_TV,dialog_pet_parent_name_TV,dialog_parent_phone_TV,dialog_parent_address_TV;
